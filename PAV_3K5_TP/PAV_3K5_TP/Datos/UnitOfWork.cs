64e187700b40b82fdfa6cfacb1c20bbdcf71009c
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-09-13 20:03:09 -0300

    Agrega implementacion del patron UnitOfWork

---
+++ PAV_3K5_TP/PAV_3K5_TP/Datos/UnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PAV_3K5_TP.Datos
+{
+    public class UnitOfWork : IDisposable
+    {
+        public string connString = "Server=localhost;Database=TPS;Trusted_Connection=True;";
+        private IDbConnection connection;
+        private IDbTransaction transaction;
+
+        public UnitOfWork()
+        {
+            connection = new SqlConnection(connString);
+        }
+
+        private UsuarioDao _usuarioDao;
+        public UsuarioDao UsuarioDao { 
+            get
+            {
+                if (_usuarioDao == null) {
+                    _usuarioDao = new UsuarioDao(connection, transaction);
+                }
+
+                return _usuarioDao;
+            }
+        }
+
+        public void Open()
+        {
+            connection.Open();
+        }
+
+        public void BeginTransaction()
+        {
+            if (connection.State != ConnectionState.Open)
+                Open();
+
+            transaction = connection.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            transaction.Commit();
+        }
+
+        public void RollBack()
+        {
+            transaction.Rollback();
+        }
+
+        public void Dispose()
+        {
+            if (transaction != null)
+                transaction.Dispose();
+
+            if (connection != null)
+                connection.Dispose();
+        }
+    }
+}
---
+++ PAV_3K5_TP/PAV_3K5_TP/Datos/UsuarioDao.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAV_3K5_TP.Datos
+{
+    public class UsuarioDao
+    {
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+
+        public UsuarioDao(IDbConnection connection, IDbTransaction transaction)
+        {
+            _connection = connection;
+            _transaction = transaction;
+        }
+
+        public void MetodoTest()
+        {
+            string sql = @"INSERT INTO USUARIOS(id_perfil,usuario,password,email,estado,borrado) " +
+                "VALUES(1,'TEST','1ASDASD','CONTACTO@GMAIL.COM','S',0)";
+
+            var affectedRows1 = _connection.Execute(sql, transaction: _transaction);
+        }
+    }
+}
